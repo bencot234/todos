0ebf6cc4c02a746f439746d4fce8b3823a3bad1d
Ben Cotton <ben@Bens-MacBook-Pro.local>
2023-01-11 12:57:42 +0000

    set value for the input to be stored in listItem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
 import List from './List';
+import { useState } from 'react';
 
 function App() {
+	const [listItem, setListItem] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('adding a list item...')
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(`adding a ${listItem}`)
+	}
 
-  return (
-    <>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <button type="submit">Add</button>
-      </form>
-      <List/>
-    </>
-  );
+	return (
+		<>
+			<h1>Todo List</h1>
+			<form onSubmit={handleSubmit}>
+				<input 
+					type="text" 
+					value={listItem} 
+					onChange={(e) => setListItem(e.target.value)}
+				/>
+				<button type="submit">Add</button>
+			</form>
+			<List/>
+		</>
+	);
 }
 
 export default App;
