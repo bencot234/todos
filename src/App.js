67f68d956d3b930e609e33e4f3bd8556f12dc489
Ben Cotton <ben@Bens-MacBook-Pro.local>
2023-01-11 14:21:01 +0000

    display message if nothing input

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import { useState } from 'react';
 function App() {
 	const [listItem, setListItem] = useState('');
 	const [list, setList] = useState([]);
+	const [message, setMessage] = useState({text: '', show: false});
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		if (listItem === '') {
+			setMessage({text: 'please enter an item', show: true});
+			setTimeout(() => {
+				setMessage({text: '', show: false});
+			}, 3000);
+			return;
+		}
 		let id = new Date().getTime().toString();
 		setList([...list, {name: listItem, id: id}]);
 		setListItem('');
@@ -17,6 +25,9 @@ function App() {
 		<>
 			<div className='title-container'>
 				<h1 className='title'>Todo List</h1>
+			</div>
+			<div className={`${message.show ? 'message-container show-message' : 'message-container'}`}>
+				<p>{message.text}</p>
 			</div>
 			<form onSubmit={handleSubmit}>
 				<div className="input-container">
